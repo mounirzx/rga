b090a23103c67db4083ae489633371826044d804
Farouk Touil <farouktouil@hotmail.com>
2024-05-03 09:13:49 +0200

    Update script for disabling/enabling form elements based on selected values
    - Disables '#type\_activite\_exploitation' when 'activite\_exploitation' is set to '1'
    - Hides '#ch

--- assets/js/logique.js
+++ assets/js/logique.js
@@ -1,29 +1,34 @@
                //make document ready 
                $(document).ready(function(){
-                 $('#type_activite_exploitation').prop('disabled', true);
+                 $('#type_activite_exploitation').prop('disabled', true);//36
                  $('#reseau_telephonique').prop('disabled', true); //39
                  
               
-                   $('#activite_exploitation').change(function() {
+                   $('#activite_exploitation').change(function() {//35
                          var selectedValue = $(this).val(); 
                          if (selectedValue === "1") {
-                               $('#chapt_animals').hide();
+                               $('#chapt_animals').hide(); //VI
+                               $('#type_activite_exploitation').prop('disabled', true);//36
                             
                          } else if (selectedValue === "2") {
-                            $('#chapt_animals').show();
-                            $('#type_activite_exploitation').prop('disabled', false);
+                            $('#chapt_animals').show();//VI
+                            $('#type_activite_exploitation').prop('disabled', false);//36
                          }else if (selectedValue === "3") {
-                               $('#element_to_hide_3').hide(); 
+                             $('#chapt_animals').show();//VI
+                             $('#type_activite_exploitation').prop('disabled', false);//36
                          }
                    });
        
-                   $('#type_activite_exploitation').change(function() {
+                   $('#type_activite_exploitation').change(function() {//36
                         var selectedValue = $(this).val(); 
                         if (selectedValue === "2") {
-                              $('#sans_terre').hide(); 
-                              $('.36_si_sont_terres').prop('hidden', true); //36
+                              $('#sans_terre').hide(); //IV
+                              $('.36_si_sont_terres').prop('hidden', true); //XII
                               
-                        } 
+                        } else if(selectedValue==="1"){
+                              $('#sans_terre').show(); //IV
+                              $('.36_si_sont_terres').prop('hidden', false); //XII
+                        }
                         
                       });
 
