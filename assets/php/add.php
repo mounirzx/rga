54e2aacbd57190dae8534c2ade147b9f9902139b
wissem2908 <115625112+wissem2908@users.noreply.github.com>
2024-05-21 11:37:37 +0100

    "Updated update.js, add.php, and update.php files with changes to display messages, calculate total hectares, and update coherence_superficie table."

--- assets/php/add.php
+++ assets/php/add.php
@@ -210,8 +210,7 @@ foreach ($formDataArrayCodeMateriel as $formData) {
 /********************************************* modification wissem 21/05/2024 10:44 ***************************************************************** */
 $message = $form->message;
 $controleSatSumsjtest2 = $form->controleSatSumsjtest2;
-print_r($message);
-print_r($controleSatSumsjtest2);
+
 $coherence_util_sol="";
 $message_coherence_util_sol="";
 $coherence_stat_jur="";
