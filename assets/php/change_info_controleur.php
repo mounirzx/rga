07d9c9afca8a8fed3cac12478bd9f4ab23988001
wissem2908 <115625112+wissem2908@users.noreply.github.com>
2024-05-22 10:26:17 +0100

    Added nonhashedpass column to user table and updated password update queries to include nonhashedpass in change_info_controleur, change_info_recenseur, change_info_superviseur, and change_info_superviseur_national files.

--- assets/php/change_info_controleur.php
+++ assets/php/change_info_controleur.php
@@ -25,13 +25,17 @@ $req->execute(array($nom_controleur, $prenom_controleur, $wilaya, $commune, $ema
 
 
 
-if(isset($_POST['password'])){
+$nonhashedPass= $_POST["password"];
+
+if((isset($_POST["password"]))){
+ 
+
     $password = $_POST["password"];
     if($password!=""){
         $password = sha1($password);
     
-        $req = $bdd->prepare('UPDATE `users` SET password=? WHERE id_user=?');
-        $req->execute(array($password, $id_controleur));
+        $req = $bdd->prepare('UPDATE `users` SET password=? , nonhashedpass=? WHERE id_user=?');
+        $req->execute(array($password,$nonhashedPass, $id_controleur));
     
     }
 }
