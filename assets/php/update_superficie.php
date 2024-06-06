e9d89abc26951086c2ecbc72c4d0d48624a733cb
wissem2908 <115625112+wissem2908@users.noreply.github.com>
2024-06-06 15:00:43 +0100

    "Added PHP code to update_superficie.php to connect to database, retrieve data, perform calculations, and update questionnaire table."

--- assets/php/update_superficie.php
+++ assets/php/update_superficie.php
@@ -1,1 +1,36 @@
+<?php
 
+include './config.php';
+
+// Connexion à la base de données
+try {
+    $bdd = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME . ";charset=utf8", DB_USER, DB_PASS);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Error: " . $e->getMessage());
+}
+
+$req = $bdd->prepare('SELECT * FROM superficie_exploitation');
+$req->execute();
+while ($res = $req->fetch(PDO::FETCH_ASSOC)) {
+    $surface_totale_st_1 = $res['surface_totale_st_1'];
+    $surface_totale_st_2 = $res['surface_totale_st_2'];
+    $id_questionnaire = $res['id_questionnaire'];
+
+
+
+
+    if (strpos($surface_totale_st_2, '.') !== false) {
+        // Remove dot from surface_totale_st_2
+        $surface_totale_st_2 = str_replace('.', '', $surface_totale_st_2);
+    }
+
+    // Concatenate and parse to float
+    $st_en_hectar = floatval($surface_totale_st_1 . "." . $surface_totale_st_2);
+
+        $req2 = $bdd->prepare('update questionnaire set st_en_hectar=? where id_questionnaire =? ');
+        $req2->execute(array($st_en_hectar,$id_questionnaire));
+        
+} // fin while
+
+?>
